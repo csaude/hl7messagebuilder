37ac6ff248a2cb7a92861de01b29ef18f14183b5
Helio Machabane <helio.machabane@gmail.com>
2023-03-12 22:09:22 +0200

    Change de module to produce HL7 files per Health Facility: 1.The name of the file must be the HF uuid 2.The HF to be produced should be configured beforehand on the disa config. table in epts

--- api/src/main/java/org/openmrs/module/hl7messagebuilder/api/dao/Hl7messagebuilderDao.java
+++ api/src/main/java/org/openmrs/module/hl7messagebuilder/api/dao/Hl7messagebuilderDao.java
@@ -84,7 +84,7 @@ public class Hl7messagebuilderDao implements Hl7messagebuilderDAO {
 		        + " where p.voided=0 and pe.voided=0 AND LENGTH(pid.identifier) = 21 AND pid.lUuid = '" + locationsByUuid
 		        + "' GROUP BY pid.identifier;";
 		
-		log.info("query..." + sql);
+		//log.info("query..." + sql);
 		
 		final Query query = this.sessionFactory.getCurrentSession().createSQLQuery(sql);
 		log.info("Querying list from database ended...");
